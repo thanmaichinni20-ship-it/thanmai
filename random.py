5c97a7d59eadfc28aefad286276ce240368175fb
thanmaichinni20-ship-it <thanmaichinni20@gmail.com>
2026-06-24 19:21:47 +0530

    Fix syntax errors and improve dice rolling logic

--- random.py
+++ random.py
@@ -1,12 +1,12 @@
 import random
-while true 
-choice=input('roll the dice ?(y/n):')lower()
+while true: 
+choice=input('roll the dice ?(y/n):').lower()
 if choice=='y':
-    die 2:random.randint(2,7)
-    die 3:random.randint(2,7)
-  print(f'{die 2},{die 3}')
+    die1:random.randint(1,6)
+    die2:random.randint(1,6)
+  print(f'{die1},{die2}')
 elif choice=='n':
    print('thanks for playing')
 break 
-else
+else:
 print('invalid choice')
