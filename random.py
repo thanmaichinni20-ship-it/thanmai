a8611f0358b7278cdcb074f63de3944d472f4d2c
thanmaichinni20-ship-it <thanmaichinni20@gmail.com>
2026-06-24 12:46:19 +0530

    Implement dice rolling functionality in random.py
    Fix syntax errors and improve dice rolling logic.

---
+++ random.py
@@ -0,0 +1,12 @@
+import random
+while true 
+choice=input('roll the dice ?(y/n):')lower()
+if choice=='y':
+    die 2:random.randint(2,7)
+    die 3:random.randint(2,7)
+  print(f'{die 2},{die 3}')
+elif choice=='n':
+   print('thanks for playing')
+break 
+else
+print('invalid choice')
