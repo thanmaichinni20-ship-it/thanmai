c95d231a95dd38a7851a5cc294c0144ae122a7c6
thanmaichinni20-ship-it <thanmaichinni20@gmail.com>
2026-06-25 12:00:30 +0530

    Change game from dice roll to number guessing
    Replaced dice rolling game with number guessing game.

--- random.py
+++ random.py
@@ -1,12 +1,18 @@
 import random
-while true: 
-choice=input('roll the dice ?(y/n):').lower()
-if choice=='y':
-    die1:random.randint(1,6)
-    die2:random.randint(1,6)
-  print(f'{die1},{die2}')
-elif choice=='n':
-   print('thanks for playing')
-break 
-else:
-print('invalid choice')
+
+number_to_guess = random.randint(1, 100)
+
+while True:
+    try:
+        guess = int(input("Guess the number between 1 and 100: "))
+
+        if guess < number_to_guess:
+            print("Too low!")
+        elif guess > number_to_guess:
+            print("Too high!")
+        else:
+            print("Congratulations! You guessed the number.")
+            break
+
+    except ValueError:
+        print("Please enter a valid number.")
